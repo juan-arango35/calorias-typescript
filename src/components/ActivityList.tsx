7456a68b55b3c583f39f952d5c3493b95cb3ed28
juan arango <juanarangozapata35@gmail.com>
2024-08-25 20:52:32 -0500

    agrega botones de edición

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { useMemo } from "react";
+import { PencilSquareIcon} from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
   activities: Activity[];
@@ -32,7 +33,13 @@ const ActivityList = ({ activities }: ActivityListProps) => {
                 {activity.calories } {' '}
                 <span>Calorias</span></p>
           </div>
-          <div></div>
+          <div className="flex gap-5 items-center">
+            <button>
+              <PencilSquareIcon
+              className="h-8 w-8 text-gray-700"
+              /> 
+            </button>
+          </div>
         </div>
       ))}
     </>
